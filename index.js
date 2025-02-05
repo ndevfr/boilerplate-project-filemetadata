a71c91115851eb78ba3b2efbd63323f1b6473200
NDev.fr <ndev@ik.me>
2025-02-05 19:57:17 +0000

    feat: add api endpoint

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const bodyParser = require('body-parser');
+require('dotenv').config();
+multer = require("multer"), 
+upload = multer({ dest: "uploads/" });
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +16,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res, next) => {
+  
+  if (!req.file.originalname) {
+    return res.send("Something went wrong");
+  }
+  const { originalname, mimetype, size } = req.file;
+  return res.json({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
